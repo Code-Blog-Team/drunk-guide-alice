7c8957873a003e98a192e4981dde6d45c8409d47
jotty <bard143games@gmail.com>
2021-10-06 12:48:11 +0300

    fix in get_answer

--- handlers/get_answer.py
+++ handlers/get_answer.py
@@ -7,14 +7,18 @@ bp = Blueprint()
 
 
 @bp.message_handler(2, ["get_answer"])
-async def get_answer(req: AliceRequest) -> AliceResponse:
+async def get_answer_handler(req: AliceRequest) -> AliceResponse:
     ans = AliceResponse()
-    answer = req.request.nlu.intents.get("get_answer")["slots"]["place"]["value"]
-    right_answer = req.state.session.get("right_answer")
-    if fuzz.partial_ratio(answer, right_answer) >= 85:
-        ans.response.text = "Вы угадали! Сыграть еще раз?"
-    else:
-        ans.response.text = "Вы не угадали... Сыграть еще раз?"
+    if req.state.session.get("current_dialog") == "wait_answer":
+        answer = req.request.nlu.intents.get("get_answer")["slots"]["place"]["value"]
+        right_answer = req.state.session.get("right_answer")
+        if fuzz.partial_ratio(answer, right_answer) >= 85:
+            ans.response.text = "Вы угадали! Сыграть еще раз?"
+        else:
+            ans.response.text = "Вы не угадали... Сыграть еще раз?"
 
-    ans.session_state["current_dialog"] = "wait_again"
+        ans.session_state["current_dialog"] = "wait_again"
+    else:
+        ans.response.text = "error TODO"
+
     return ans
